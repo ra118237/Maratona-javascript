be83a454d13ce0c15d16ad0a8fc3df150435c74c
John William <ra118237@uem.br>
2021-03-04 11:51:48 -0300

    Os diferentes tipos de comentarios

---
+++ Basico/02- Comentários/Comentarios.js
@@ -0,0 +1,35 @@
+/*
+  Comentários são ignorados pelo motor javascript, interpretador da linguagem. Dessa maneira, eessa parte do código não é levada em conta !
+*/
+
+// Tipos de comentários:
+
+// Comentários de uma linha
+
+/*
+  Comentários que se extendem para várias linhas.
+*/
+
+//Exemplos :
+
+var numero = 118237;//a variavel foi atribuida com valor 118237
+
+console.log(numero);//numero foi exibida no console
+
+/*
+  Função que retorna a soma de dois numeros:
+  Parametros: Duas variaveis
+*/
+function sum(a,b){
+  return a+b;
+}
+
+
+
+/*
+  Comentários podem ser uteis para esconder códigos indesejados...
+*/
+
+var x=4;
+//x=45;
+console.log(x);
