2dcc026142fa29689c717474f9c971fb5d130d73
John William <ra118237@uem.br>
2021-03-04 11:52:18 -0300

    Tipos de dados

---
+++ Basico/03- Tipos de Dados/tiposDeDados.js
@@ -0,0 +1,15 @@
+ 
+/*
+  Tipos de dados são representações abstratas do mundo real na qual uma linguagem consegue fazer determinadas operações com os valores destes.
+  O javascript fornece alguns tipos de dados para serem usados:
+  
+  undefined: É um valor não definido;
+  null: Valor esse que representa um valor nulo;
+  boolean: Representa todos os valores lógicos, ou seja, true(verdadeiro) ou false(falso);
+  string: Representa uma sequencia de caracteres juntos como uma frase ou palavra, exemplo: "John", "Senhor dos aneis é um ótimo filme";
+  symbol: São valores imutáveis;
+  number: Representa número inteiros ou reais como: 1,2,4.5,7...;
+  object: São valores que representam um conjunto heterogênio de outros valores.
+
+*/
+
