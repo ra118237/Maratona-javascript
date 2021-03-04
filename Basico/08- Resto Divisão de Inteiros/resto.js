209da269255cbe7178985a06de378849489e2c10
John William <ra118237@uem.br>
2021-03-04 11:54:21 -0300

    Resto da Divisao

---
+++ Basico/08- Resto Divisão de Inteiros/resto.js
@@ -0,0 +1,12 @@
+//O operador '%' devolve o resto da divisão de dois inteiros
+var numero = 42;
+
+var restoDaDivisao = numero%3;
+
+console.log(restoDaDivisao);
+
+restoDaDivisao = numero%5;
+
+
+console.log(restoDaDivisao);
+
