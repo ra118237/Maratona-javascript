edab254e53abdb63c8d5da1aa1678101fa9a0e22
John William <ra118237@uem.br>
2021-03-04 11:54:03 -0300

    ncremento e decremento rapido

---
+++ Basico/07- Incremento e decremento/IncrementoDecremento.js
@@ -0,0 +1,11 @@
+
+var numero = 14;
+
+numero = numero + 1 // Acrescenta 1 ao valor do numero
+
+numero++;//Faz o mesmo que a linha de cima
+
+numero = numero - 1 // Decrementa 1 ao valor do numero
+
+numero--;//Faz o mesmo que a linha de cima
+
